c4f06964c82248f0c8e06bebe63544866c397693
evghn <evghn94_git@mail.ru>
2026-01-09 20:01:20 +0300

    update dump db

--- shop_9isp_221.sql
+++ shop_9isp_221.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: MySQL-8.4
--- Время создания: Ноя 22 2025 г., 14:57
+-- Время создания: Янв 09 2026 г., 19:59
 -- Версия сервера: 8.4.6
 -- Версия PHP: 8.3.25
 
@@ -34,6 +34,13 @@ CREATE TABLE `cart` (
   `total` decimal(10,0) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Дамп данных таблицы `cart`
+--
+
+INSERT INTO `cart` (`id`, `user_id`, `amount`, `total`) VALUES
+(1, 1, 26, 2900);
+
 -- --------------------------------------------------------
 
 --
@@ -48,6 +55,17 @@ CREATE TABLE `cart_item` (
   `price` decimal(10,0) NOT NULL DEFAULT '0',
   `total` decimal(10,0) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Дамп данных таблицы `cart_item`
+--
+
+INSERT INTO `cart_item` (`id`, `cart_id`, `product_id`, `amount`, `price`, `total`) VALUES
+(3, 1, 2, 7, 100, 700),
+(7, 1, 9, 2, 150, 300),
+(8, 1, 6, 5, 100, 500),
+(9, 1, 3, 1, 150, 150),
+(10, 1, 4, 1, 100, 100);
 
 -- --------------------------------------------------------
 
@@ -70,6 +88,31 @@ INSERT INTO `category` (`id`, `title`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Структура таблицы `favourite`
+--
+
+CREATE TABLE `favourite` (
+  `id` int UNSIGNED NOT NULL,
+  `product_id` int UNSIGNED NOT NULL,
+  `user_id` int UNSIGNED NOT NULL,
+  `status` tinyint UNSIGNED NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Дамп данных таблицы `favourite`
+--
+
+INSERT INTO `favourite` (`id`, `product_id`, `user_id`, `status`) VALUES
+(2, 3, 1, 1),
+(3, 4, 1, 0),
+(4, 5, 1, 0),
+(5, 2, 1, 1),
+(6, 7, 1, 1),
+(7, 6, 1, 1);
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблицы `order`
 --
 
@@ -82,6 +125,13 @@ CREATE TABLE `order` (
   `status_id` int UNSIGNED NOT NULL,
   `pay_type_id` int UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Дамп данных таблицы `order`
+--
+
+INSERT INTO `order` (`id`, `amount`, `total`, `user_id`, `created_at`, `status_id`, `pay_type_id`) VALUES
+(7, 26, 2900, 1, '2025-12-27 11:29:24', 4, 1);
 
 -- --------------------------------------------------------
 
@@ -98,6 +148,17 @@ CREATE TABLE `order_item` (
   `total` decimal(10,0) UNSIGNED NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Дамп данных таблицы `order_item`
+--
+
+INSERT INTO `order_item` (`id`, `order_id`, `product_id`, `amount`, `price`, `total`) VALUES
+(1, 7, 2, 7, 100, 700),
+(2, 7, 9, 2, 150, 300),
+(3, 7, 6, 5, 100, 500),
+(4, 7, 3, 1, 150, 150),
+(5, 7, 4, 1, 100, 100);
+
 -- --------------------------------------------------------
 
 --
@@ -108,6 +169,14 @@ CREATE TABLE `pay_type` (
   `id` int UNSIGNED NOT NULL,
   `title` varchar(255) COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Дамп данных таблицы `pay_type`
+--
+
+INSERT INTO `pay_type` (`id`, `title`) VALUES
+(1, 'Наличные'),
+(2, 'Банковская карта');
 
 -- --------------------------------------------------------
 
@@ -121,22 +190,25 @@ CREATE TABLE `product` (
   `amount` int NOT NULL DEFAULT '0',
   `price` decimal(10,0) NOT NULL DEFAULT '0',
   `category_id` int UNSIGNED NOT NULL,
-  `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci
+  `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
+  `like` int UNSIGNED NOT NULL DEFAULT '0',
+  `dislike` int UNSIGNED NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Дамп данных таблицы `product`
 --
 
-INSERT INTO `product` (`id`, `title`, `amount`, `price`, `category_id`, `description`) VALUES
-(2, 'молоко', 101, 100, 1, ''),
-(3, 'молоко2', 10, 150, 1, 'вкусное молоко от коровы '),
-(4, 'молоко3', 10, 100, 1, 'молоко3'),
-(5, 'молоко4', 10, 150, 1, 'молоко4'),
-(6, 'молоко5', 10, 100, 1, ''),
-(7, 'молоко6', 10, 150, 1, 'молоко6'),
-(8, 'молоко7', 10, 100, 1, 'молоко7'),
-(9, 'молоко8', 10, 150, 1, 'молоко8');
+INSERT INTO `product` (`id`, `title`, `amount`, `price`, `category_id`, `description`, `like`, `dislike`) VALUES
+(2, 'молоко', 101, 100, 1, '', 1, 0),
+(3, 'молоко2', 10, 150, 1, 'вкусное молоко от коровы ', 1, 0),
+(4, 'молоко3', 10, 100, 1, 'молоко3', 0, 0),
+(5, 'молоко4', 10, 150, 1, 'молоко4', 0, 0),
+(6, 'молоко5', 10, 100, 1, '', 0, 0),
+(7, 'молоко6', 10, 150, 1, 'молоко6', 0, 0),
+(8, 'молоко7', 10, 100, 1, 'молоко7', 1, 0),
+(9, 'молоко8', 10, 150, 1, 'молоко8', 0, 1),
+(10, 'молоко тест', 3, 100, 1, '<div style=\"text-align: justify;\"><span style=\"font-size:12px\"><span style=\"color:#FF8C00\"><strong>dfgdsfgsd</strong></span><span style=\"color:#000000\"><strong><span style=\"background-color:#EE82EE\">&nbsp; &nbsp; &nbsp; fghfghfs</span></strong></span></span></div>\r\n\r\n<div style=\"text-align: justify;\"><span style=\"font-size:12px\"><span style=\"color:#FF8C00\"><strong><span style=\"background-color:#EE82EE\">fg</span></strong></span></span></div>\r\n\r\n<div style=\"text-align: justify;\"><span style=\"font-size:12px\"><span style=\"color:#FF8C00\"><strong>sdf</strong></span></span></div>\r\n\r\n<div style=\"text-align: justify;\"><span style=\"font-size:12px\"><span style=\"color:#FF8C00\"><strong>g</strong></span><span style=\"color:#FF8C00\"><strong><img alt=\"\" src=\"/img/milk.jpg\" style=\"height:116px; width:116px\" /></strong></span></span></div>\r\n\r\n<div style=\"text-align: justify;\"><span style=\"font-size:12px\"><span style=\"color:#FF8C00\"><strong>sdf</strong></span></span></div>\r\n\r\n<div style=\"text-align: justify;\"><span style=\"font-size:12px\"><span style=\"color:#FF8C00\"><strong>gsdfg<img alt=\"\" src=\"/img//home.jpeg\" style=\"height:94px; width:150px\" /></strong></span></span></div>\r\n\r\n<p>&nbsp;</p>\r\n', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -162,7 +234,8 @@ INSERT INTO `product_image` (`id`, `product_id`, `photo`) VALUES
 (5, 5, '2_1763802666_Y2_MVtWr87BMBEq9A9j3wnDMjuFvXv-6.jpg'),
 (6, 6, '2_1763803315_y75zisWVmiryS3IXUcNr6LV5r5PUjAut.jpg'),
 (7, 7, '2_1763802666_Y2_MVtWr87BMBEq9A9j3wnDMjuFvXv-6.jpg'),
-(8, 8, '2_1763803315_y75zisWVmiryS3IXUcNr6LV5r5PUjAut.jpg');
+(8, 8, '2_1763803315_y75zisWVmiryS3IXUcNr6LV5r5PUjAut.jpg'),
+(10, 10, '2_1766834601_jIvF2upy6_XKLNmfwpZs0SPGYRJMIvVA.jpg');
 
 -- --------------------------------------------------------
 
@@ -172,8 +245,19 @@ INSERT INTO `product_image` (`id`, `product_id`, `photo`) VALUES
 
 CREATE TABLE `status` (
   `id` int UNSIGNED NOT NULL,
-  `title` varchar(255) COLLATE utf8mb4_general_ci NOT NULL
+  `title` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `alias` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Дамп данных таблицы `status`
+--
+
+INSERT INTO `status` (`id`, `title`, `alias`) VALUES
+(1, 'Новый', 'new'),
+(2, 'Удаленный', 'delete'),
+(3, 'Завершен', 'final'),
+(4, 'В работе', 'in-working');
 
 -- --------------------------------------------------------
 
@@ -196,8 +280,33 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `login`, `password`, `name`, `email`, `auth_key`, `role`) VALUES
-(1, 'q', '$2y$13$fS7XPX.mtRlXycP73PFxEe7EMtibjkTlDxcypGCO50oX1oHvdrrR2', 'й', 'q@q.q', 'HEWh3VhCBTHSsw6c0c1nmHk6yqtSvig2', 0),
+(1, 'q', '$2y$13$fS7XPX.mtRlXycP73PFxEe7EMtibjkTlDxcypGCO50oX1oHvdrrR2', 'й', 'iv2-22-web@mail.ru', 'HEWh3VhCBTHSsw6c0c1nmHk6yqtSvig2', 0),
 (2, 'admin', '$2y$13$8e828x1DOl6.CaDGBxS7jOH2JZzg6gue3dpwdvOI1TEwEAOCCnsVS', 'admin', 'admin@admin', 'iim8rmcIYXWj7NDYFTpTSakcqyiKZnjM', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `user_reaction`
+--
+
+CREATE TABLE `user_reaction` (
+  `id` int UNSIGNED NOT NULL,
+  `user_id` int UNSIGNED NOT NULL,
+  `product_id` int UNSIGNED NOT NULL,
+  `status` tinyint UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Дамп данных таблицы `user_reaction`
+--
+
+INSERT INTO `user_reaction` (`id`, `user_id`, `product_id`, `status`) VALUES
+(1, 1, 5, NULL),
+(2, 1, 4, NULL),
+(3, 1, 8, 1),
+(4, 1, 9, 0),
+(5, 1, 3, 1),
+(6, 1, 2, 1);
 
 --
 -- Индексы сохранённых таблиц
@@ -223,6 +332,14 @@ ALTER TABLE `cart_item`
 --
 ALTER TABLE `category`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `favourite`
+--
+ALTER TABLE `favourite`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `product_id` (`product_id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Индексы таблицы `order`
@@ -274,6 +391,14 @@ ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Индексы таблицы `user_reaction`
+--
+ALTER TABLE `user_reaction`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `product_id` (`product_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
 -- AUTO_INCREMENT для сохранённых таблиц
 --
 
@@ -281,13 +406,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT для таблицы `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT для таблицы `cart_item`
 --
 ALTER TABLE `cart_item`
-  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT для таблицы `category`
@@ -296,46 +421,58 @@ ALTER TABLE `category`
   MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT для таблицы `favourite`
+--
+ALTER TABLE `favourite`
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
 -- AUTO_INCREMENT для таблицы `order`
 --
 ALTER TABLE `order`
-  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT для таблицы `order_item`
 --
 ALTER TABLE `order_item`
-  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT для таблицы `pay_type`
 --
 ALTER TABLE `pay_type`
-  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT для таблицы `product`
 --
 ALTER TABLE `product`
-  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT для таблицы `product_image`
 --
 ALTER TABLE `product_image`
-  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT для таблицы `status`
 --
 ALTER TABLE `status`
-  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT для таблицы `user`
 --
 ALTER TABLE `user`
   MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT для таблицы `user_reaction`
+--
+ALTER TABLE `user_reaction`
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
@@ -353,6 +490,13 @@ ALTER TABLE `cart`
 ALTER TABLE `cart_item`
   ADD CONSTRAINT `cart_item_ibfk_1` FOREIGN KEY (`cart_id`) REFERENCES `cart` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
   ADD CONSTRAINT `cart_item_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Ограничения внешнего ключа таблицы `favourite`
+--
+ALTER TABLE `favourite`
+  ADD CONSTRAINT `favourite_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `favourite_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 --
 -- Ограничения внешнего ключа таблицы `order`
@@ -380,6 +524,13 @@ ALTER TABLE `product`
 --
 ALTER TABLE `product_image`
   ADD CONSTRAINT `product_image_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Ограничения внешнего ключа таблицы `user_reaction`
+--
+ALTER TABLE `user_reaction`
+  ADD CONSTRAINT `user_reaction_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `user_reaction_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
